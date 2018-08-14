3516f691a9a62a74265ed783a137222d99e9805d
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.local>
2018-08-14 13:39:00 -0400

    barebones drawing

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+var hello = "hello world";
+
+console.log("hi");
+
+
+});
+
+function setup() {
+    createCanvas(640,480);
+    }
+    
+    function draw() {
+        background(0,0,0,20);
+        if (mouseIsPressed) {
+            fill(0);
+          } else {
+            fill(255);
+          }
+          ellipse(mouseX, mouseY, 80, 80);
+    }
+    
