39ad834e31fb713ba59ede1f2c78053da4bfefd1
Jdelorim <joshuadelorimier@gmail.com>
2018-08-14 15:41:12 -0400

    trying to deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+var PORT = 4040;
+var server = app.listen(PORT,listening);
+
+function listening() {
+    console.log("listening on:",PORT);
+}
+
+app.use(express.static("public"));
