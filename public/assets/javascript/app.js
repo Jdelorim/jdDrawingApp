254c9ae65b55aa76eb42cb840f7681affc36bff2
Jdelorim <joshuadelorimier@gmail.com>
2018-08-15 15:29:43 -0400

    added clear(wonky) and local save buttons and functions

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,29 +1,41 @@
-$(document).ready(function() {
-var hello = "hello world";
 
-console.log(hello);
-
-
-});
-//---------------------------------------------
-var didMove = false;
 var x = 0;
 var c;
-//------------------------------------
+var saveME = false;
+var clearME = false;
+var alphadog = 255;
+var firstTime = true;
+
+//-----------jquery-------
+$(document).ready(function() {
+   
+    var hello = "hello world";
+  
+    $("#jdSave").on("click", function() {
+        saveME = true;
+    });
+    $("#jdClear").on("click",function(){
+        clearME = true; 
+        console.log("clearME: ", clearME);
+    });
+});
+//--------p5-----------------
 function setup() {
     c = createCanvas(600,400);
-
+   
 }
-//-------------------------------------------------    
+  
 function draw() {
-   background(0,0,0,0);
-
-
+  
+   background(0,0,0,alphadog);
+   saveLocal();
+   clearMe();
+   
 }
 
 function mouseDragged() {
     
-   if(x<10) {
+    if(x<10) {
          x++;
     } else {
          x=0;   
@@ -33,10 +45,29 @@ function mouseDragged() {
    noStroke();
    ellipse(mouseX, mouseY, x, x);
 }
-function keyPressed(){
-    if(keyCode === 32){
+
+//-----------my functions--------
+function saveLocal() {
+    if(saveME === true) {
         saveCanvas(c, "joshtest", "png")
-        console.log("you hit da space bar!");
+        console.log("you saved the picture!");
+        saveME = false;
+    }
+}
+
+function clearMe() {
+    
+    if(clearME === true) {
+        
+        alphadog = 255;
+
+        console.log("you hit clear, alphadog:", alphadog);
+        setTimeout(function() {
+        alphadog = 0;
+        console.log("alphadog: ",alphadog);
+       }, 1000);
+        clearME = false;
+       
     }
 }
 
